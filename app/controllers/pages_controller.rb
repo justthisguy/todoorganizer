3eaaac5dbd329bce9bc09ed69b55973487b64f05
Ken Decanio <ken@41monkeys.com>
2021-11-29 21:46:59 -0800

    adds default pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,25 @@
+class PagesController < ApplicationController
+  # before_filter :authenticate_user!, only: [:home]
+  def home
+    @foo = "You are on your home page"
+  end
+
+  def welcome
+    @foo = "Sign in or sign up"
+    @foo = " With SXSW Friends"  if request.subdomain == "sxsw"
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  # Terms of service
+  def terms
+  end
+
+  def privacy
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  ###
+  # Common pages *
+  ###
+  get "home"    => "pages#home",    as: "home"
+  get "welcome" => "pages#welcome", as: "welcome"
+  get "about"   => "pages#about",   as: "about"
+  get "contact" => "pages#contact", as: "contact"
+  get "terms"   => "pages#terms",   as: "terms"
+  get "privacy" => "pages#privacy", as: "privacy"
+
+  ###
+  # root path depends on authentication
+  ###
+  # authenticated :user do
+  #   root to: "pages#home", as: :authenticated_root
+  # end
+
+  # unauthenticated do
+    root to: "pages#welcome"
+  # end
 end
