4429258ea9828a605be71b2853ade8dc4bfab73f
Ken Decanio <github.com@41monkeys.com>
2022-07-22 19:35:25 -0700

    adds creation from_hash to Wunderfolder

--- app/models/wunderfolder.rb
+++ app/models/wunderfolder.rb
@@ -1,3 +1,25 @@
 class Wunderfolder < ApplicationRecord
   has_many :wunderlists
+
+  def from_hash source
+    self.title = source['title']
+    self.directory_path = source['directoryPath']
+    self.createdAt = DateTime.parse source['createdAt']
+    self.updatedAt = DateTime.parse source['updatedAt']
+    self.save
+  end
+
+  # def self.find_or_create source
+  #   folder = Wunderfolder.where( title: source['title'] )
+  #   if folder.empty?
+  #     folder =  Wunderfolder.new
+  #     folder.from_hash source
+  #   end
+  #   folder
+  # end
 end
+
+
+
+# hf1 = {"title"=>"TODO", "directoryPath"=>"TODO", "createdAt"=>"2016-12-19T18:15:51.952Z", "updatedAt"=>"2019-08-18T03:48:55.746Z"}
+# hf2 = {"title"=>"test3", "directoryPath"=>"test3", "createdAt"=>"2016-12-19T18:15:51.952Z", "updatedAt"=>"2019-08-18T03:48:55.746Z"}
--- spec/models/wunderfolder_spec.rb
+++ spec/models/wunderfolder_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Wunderfolder, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should have_many(:wunderlists) }
+  end
+
+  describe 'from hash' do
+    let(:hash_folder) { {"title"=>"TODO", "directoryPath"=>"the directory path", "createdAt"=>"2016-12-19T18:15:51.952Z", "updatedAt"=>"2019-08-18T03:48:55.746Z"} }
+    let(:folder) { Wunderfolder.new }
+
+    context 'is not completed' do
+      before(:each) do
+        folder.from_hash hash_folder
+      end
+
+      it 'sets title to the title' do
+        expect(folder.title).to eq(hash_folder["title"])
+      end
+
+      it 'sets completed to the directory_path' do
+        expect(folder.directory_path).to eq(hash_folder["directoryPath"])
+      end
+
+      it 'sets createdAt to the createdAt' do
+        expect(folder.createdAt).to eq(hash_folder["createdAt"])
+      end
+
+      it 'sets completedAt to the updatedAt' do
+        expect(folder.updatedAt).to eq(hash_folder["updatedAt"])
+      end
+    end
+  end
 end
