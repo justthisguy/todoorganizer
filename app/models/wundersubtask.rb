772aa16489e163aa4e0df371adebd1cd6526ab59
Ken Decanio <github.com@41monkeys.com>
2022-07-24 15:04:29 -0700

    adds from_hash to Wundersubtask

--- app/models/wundersubtask.rb
+++ app/models/wundersubtask.rb
@@ -1,3 +1,17 @@
 class Wundersubtask < ApplicationRecord
   belongs_to :wundertask
+
+  def from_hash source
+    self.title = source[:title]
+    self.completed = source[:completed]
+    self.createdAt = DateTime.parse source[:createdAt]
+    self.completedAt = source[:completedAt].nil? ? nil : (DateTime.parse source[:completedAt])
+
+    self.save
+    self
+  end
 end
+
+# hst1 = { "title": "what needs doing", "completed": false, "completedAt": nil, "createdAt": "2018-07-07T20:03:51.994Z", "createdBy": { "id": 1344494, "name": "wunderlist@justthisguy.org", "email": "wunderlist@justthisguy.org", "createdAt": "2011-10-04T03:16:24Z" } }
+# hst2 = { "title": "what's been done", "completed": true, "completedAt": "2018-07-31T16:22:52.731Z", "createdAt": "2018-07-07T20:03:51.994Z", "createdBy": { "id": 1344494, "name": "wunderlist@justthisguy.org", "email": "wunderlist@justthisguy.org", "createdAt": "2011-10-04T03:16:24Z" } }
+
--- spec/models/wundersubtask_spec.rb
+++ spec/models/wundersubtask_spec.rb
@@ -1,5 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe Wundersubtask, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:wundertask) }
+  end
+
+  describe 'from hash' do
+    let(:tast) { FactoryBot.create :wundertask }
+    let(:hst1) { { "title": "what needs doing", "completed": false, "completedAt": nil, "createdAt": "2018-07-07T20:03:51.994Z", "createdBy": { "id": 1344494, "name": "wunderlist@justthisguy.org", "email": "wunderlist@justthisguy.org", "createdAt": "2011-10-04T03:16:24Z" } } }
+    let(:hst2) { { "title": "what's been done", "completed": true,  "completedAt": "2018-07-31T16:22:52.731Z", "createdAt": "2018-07-07T20:03:51.994Z", "createdBy": { "id": 1344494, "name": "wunderlist@justthisguy.org", "email": "wunderlist@justthisguy.org", "createdAt": "2011-10-04T03:16:24Z" } } }
+    let(:date) { DateTime.parse "2018-07-07T20:03:51.994Z" }
+    let(:subtask) { tast.wundersubtasks.create }
+
+    context 'is not completed' do
+      before(:each) do
+        subtask.from_hash hst1
+      end
+
+      it 'sets title to the title' do
+        expect(subtask.title).to eq( hst1[:title] )
+      end
+
+      it 'sets createdAt to the createdAt' do
+        expect(subtask.createdAt).to eq( DateTime.parse hst1[:createdAt] )
+      end
+
+      it 'sets completed to false' do
+        expect(subtask.completed).to be false
+      end
+
+      it 'sets completedAt to null' do
+        expect(subtask.completedAt).to be_nil
+      end
+    end
+
+    context 'is completed' do
+      before(:each) do
+        subtask.from_hash hst2
+      end
+
+      it 'sets title to the title' do
+        expect(subtask.title).to eq( hst2[:title] )
+      end
+
+      it 'sets createdAt to the createdAt' do
+        expect(subtask.createdAt).to eq( DateTime.parse hst2[:createdAt] )
+      end
+
+      it 'sets completed to false' do
+        expect(subtask.completed).to be true
+      end
+
+      it 'sets completedAt to completedAt' do
+        expect(subtask.completedAt).to eq( DateTime.parse hst2[:completedAt] )
+      end
+    end
+  end
 end
