4c76bc8c560121caab6e8e257627d757fee6bc90
Ken Decanio <github.com@41monkeys.com>
2022-07-24 13:28:11 -0700

    adds creation from_hash to Wundertask

--- app/models/wundertask.rb
+++ app/models/wundertask.rb
@@ -3,6 +3,78 @@ class Wundertask < ApplicationRecord
   has_many :wundersubtasks
 
   def from_hash source
-    return nil
+    self.original_id = source[:id]
+    self.title       = source[:title]
+    self.completed   = source[:completed]
+    self.starred     = source[:starred]
+    self.createdAt   = DateTime.parse source[:createdAt]
+    self.completedAt = source[:completedAt].nil? ? nil : (DateTime.parse source[:completedAt])
+
+    self.save
+    self
   end
+
+  def handle_notes notes_array
+
+  end
+
+  def handle_subtasks subtasks_array
+
+  end
+
+  def handle_files files_array
+
+  end
+
 end
+
+
+
+
+# { "id": 4158461911, "title": "vinegar", "completed": true, "starred": false, "subtasks": [], "notes": [], "comments": [], "reminders": [], "files": [], "assignee": null, "dueDate": null, "createdAt": "2018-08-25T01:34:54.968Z", "createdBy": { "id": 1344494, "name": "wunderlist@justthisguy.org", "email": "wunderlist@justthisguy.org", "createdAt": "2011-10-04T03:16:24Z" }, "completedAt": "2019-07-05T21:47:57.425Z", "completedBy": { "id": 1344494, "name": "wunderlist@justthisguy.org", "email": "wunderlist@justthisguy.org", "createdAt": "2011-10-04T03:16:24Z" } }
+
+
+
+# "subtasks": [], see subtasks
+# "notes": [],
+       # "notes": [
+       #          {
+       #            "content": "https://www.buzzfeed.com/alexkantrowitz/how-twitter-made-the-tech-worlds-most-unlikely-comeback\n"
+       #          }
+       #        ],
+        # "notes": [
+        #   {
+        #     "content": ""
+        #   }
+        # ],
+
+
+# "comments": [],  no comments
+# "reminders": [], ignore
+
+
+# "files": [],
+
+        # "files": [
+        #   {
+        #     "id": "63527455",
+        #     "fileName": "QuakeFeed Privacy Policy and Terms of Service.pdf",
+        #     "filePath": "Tech/QuakeFeed Privacy Policy and Terms of Service.pdf",
+        #     "fileSize": 199921,
+        #     "downloadUrl": "https://download.wunderlist.io/13573110-80c9-0137-d683-22000b0b2373-1562272482-34993?AWSAccessKeyId=AKIAJEN6W4AO3LJODOAA&Expires=1582254173&Signature=YgdQPLnbl7v0Ej87qDONgQej4Vs%3D",
+        #     "isDropboxFile": false,
+        #     "user": {
+        #       "id": 1344494,
+        #       "name": "wunderlist@justthisguy.org",
+        #       "email": "wunderlist@justthisguy.org",
+        #       "createdAt": "2011-10-04T03:16:24Z"
+        #     },
+        #     "localCreatedAt": "2019-07-04T20:34:44.365Z",
+        #     "createdAt": "2019-07-04T20:34:44.544Z",
+        #     "updatedAt": "2020-01-21T03:02:53.323Z"
+        #   }
+        # ],
+
+
+
+
--- spec/models/wundertask_spec.rb
+++ spec/models/wundertask_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe Wundertask, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to(:wunderlist) }
+    it { should have_many(:wundersubtasks) }
+  end
+
+  describe 'from hash' do
+    context 'always' do
+      let(:task) { Wundertask.new }
+      let(:hash_task) { { "id": 4158461911, "title": "vinegar", "completed": true, "starred": false, "subtasks": [], "notes": [], "comments": [], "reminders": [], "files": [], "assignee": nil, "dueDate": nil, "createdAt": "2018-08-25T01:34:54.968Z", "createdBy": { "id": 1344494, "name": "wunderlist@justthisguy.org", "email": "wunderlist@justthisguy.org", "createdAt": "2011-10-04T03:16:24Z" }, "completedAt": "2019-07-05T21:47:57.425Z", "completedBy": { "id": 1344494, "name": "wunderlist@justthisguy.org", "email": "wunderlist@justthisguy.org", "createdAt": "2011-10-04T03:16:24Z" } } }
+
+      before(:each) do
+        task.from_hash hash_task
+      end
+
+      it 'sets original_id to the it' do
+        expect(task.original_id).to eq(hash_task[:id])
+      end
+
+      it 'sets title to the title' do
+        expect(task.title).to eq(hash_task[:title])
+      end
+
+      it 'sets completed to the completed' do
+        expect(task.completed).to eq(hash_task[:completed])
+      end
+
+      it 'sets starred to the starred' do
+        expect(task.starred).to eq(hash_task[:starred])
+      end
+
+      it 'sets createdAt to the createdAt' do
+        expect(task.createdAt).to eq(hash_task[:createdAt])
+      end
+
+      it 'sets completedAt to the completedAt' do
+        expect(task.completedAt).to eq(hash_task[:completedAt])
+      end
+    end
+  end
+
+
+
+
+
 end
