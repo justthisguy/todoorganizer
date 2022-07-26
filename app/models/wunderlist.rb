55f86b1db084e79b54b253778df71bf0117e671b
Ken Decanio <github.com@41monkeys.com>
2022-07-25 19:01:44 -0700

    adds utility to clean out the database
    should live some place else

--- app/models/wunderlist.rb
+++ app/models/wunderlist.rb
@@ -37,6 +37,14 @@ class Wunderlist < ApplicationRecord
       Wunderlist.create_from_hash list_hash
     end
   end
+
+  def self.deleteall # needs to be done in reverse order of dependancy
+    Wundersubtask.delete_all ; puts Wundersubtask.count
+    Wundertask.delete_all    ; puts Wundertask.count
+    Wunderlist.delete_all    ; puts Wunderlist.count
+    Wunderfolder.delete_all  ; puts Wunderfolder.count
+  end
+
 end
 
 # { "id": 34496580, "title": "7 Stills", "folder": nil, "tasks": [], "directoryPath": "7 Stills", "textFilePath": "7 Stills/7 Stills.txt", "htmlFilePath": "7 Stills/7 Stills.html", "createdAt": "2012-12-06T04:51:13.98Z" }
